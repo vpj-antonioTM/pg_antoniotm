baec4347006f8e2f18e4435fb0b1839904e2a99c
1DAM - Antonio Torres Martin <atorresm11@iesvjp.es>
2025-10-16 15:28:13 +0200

    hecho

--- Tema-3/Ejercicios Opcionales/Ejercicio-05/src/ejercicio/pkg05/Ejercicio05.java
+++ Tema-3/Ejercicios Opcionales/Ejercicio-05/src/ejercicio/pkg05/Ejercicio05.java
@@ -14,16 +14,14 @@ public class Ejercicio05 {
             Scanner dato1 = new Scanner (System.in);
             System.out.println("Por favor, introduzca un numero: ");
             int num1 = dato1.nextInt();
+            
             //Esta estructura condicional va a coger el numero que ha introducido el usuario y lo va a dividir entre 2, dependiendo si el resultado es 0 o 1, imprimira si es par o impar
             if (num1 % 2 == 0 ){
            System.out.println( "Este numero es par =  " + num1);
-       }
-       else {
+            }
+            else {
            System.out.println("Este numero es impar  = " + num1);
-       }
-               
-      
-       
+            }
     }
     
 }
