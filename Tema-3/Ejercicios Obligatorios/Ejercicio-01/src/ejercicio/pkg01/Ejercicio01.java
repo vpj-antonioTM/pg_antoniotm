31e2e073f89248dcf6e4907fd6b13cf27a51f375
1DAM - Antonio Torres Martin <atorresm11@iesvjp.es>
2025-10-27 20:48:05 +0100

    cambios

--- Tema-3/Ejercicios Obligatorios/Ejercicio-01/src/ejercicio/pkg01/Ejercicio01.java
+++ Tema-3/Ejercicios Obligatorios/Ejercicio-01/src/ejercicio/pkg01/Ejercicio01.java
@@ -16,14 +16,18 @@ public class Ejercicio01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //Creo una clase Scanner para que le pida al usuario que introduzca un numero
        Scanner dato = new Scanner (System.in);
        
+       //Creo el mensaje que le salta al usuario y tambien añado una variable para numero
        System.out.println("Por favor, introduzca un numero: ");
        int numero = dato.nextInt();
        
+       //Creo un if y le digo que aquel numero que sea mayor o igual que 0 imprima el mensaje positivo
        if (numero >= 0 ){
            System.out.println( "El numero introducido es positivo");
        }
+       //Y hago un else para que todos los numeros que no cumplan lo que pide if, le imprima negativo
        else {
            System.out.println("El numero introducido es negativo");
        }
